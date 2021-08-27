c257d21ae0342612aca5b5c8258170f915e313bf
Nikolay Rademacher <hrrdx33@gmail.com>
2021-08-27 04:14:51 +0200

    feat: add search input styling

--- src/components/Filters/SearchInput.tsx
+++ src/components/Filters/SearchInput.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { Fzf } from 'fzf';
+import styles from './SearchInput.module.scss';
 
 export const SearchInput = ({
   countries,
@@ -37,5 +38,28 @@ export const SearchInput = ({
     }
   };
 
-  return <input type="text" value={query} onChange={handleChange} />;
+  return (
+    <section className={styles.search}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={styles.icon}
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+      <input
+        className={styles.input}
+        type="text"
+        value={query}
+        onChange={handleChange}
+      />
+    </section>
+  );
 };
